0bc06714825f83c96527692ce7299fb8a2c524b1
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-08 03:54:17 -0300

    build: mongo dependencies

--- Backend/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize")
-const sequelize = new Sequelize("lendingspace", "root", "lendingroot", {
-    dialect: "mysql",
-    host: "localhost",
-    post: "3306"
-})
-
-module.exports = sequelize
--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,27 @@
-const port = 55155 || process.env.port
+const port =  process.env.PORT || 55155
+
+const mongoose = require("mongoose")
+const cors = require("cors")
+
 const express = require("express")
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-const database = require("db")
-await database.sync()
+const uri = "mongodb://127.0.0.1:27017/lendingdb"
 
-const User = require("./model/User")
+function connect(){
+    try{
+        mongoose.connect(uri)
+        console.log("Connected to MongoDB!")
+    } catch(error){
+        console.error(error)
+    }
+}
 
+connect()
 
 app.listen(port, () => {
+    
     console.log(`Server listening on port ${port}`)
 })
